5ce1283223454a59b5be1a45b0acb65e43fee05a
rapuhotelli <jukka@lahetkangas.net>
2018-05-21 00:09:34 +0300

    withPose

--- client/src/components/Items/index.js
+++ client/src/components/Items/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./griditem.pcss";
 import classNames from "classnames";
-import posed from "react-pose";
+import withPose from "../withPose";
 
 import HN from "./HN";
 import Tweet from "./Tweet";
@@ -21,43 +21,15 @@ const chooseComponent = props => {
   }
 };
 
-const Pose = posed.div({
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
-  initialPose: "hidden"
-});
+const Item = props => {
+  console.log(props);
+  return (
+    <div className={classNames(style.gridItem, style.youtube)}>
+      {chooseComponent(props)}
+    </div>
+  );
+};
 
-class Item extends React.Component {
-  state = {
-    isVisible: false
-  };
+// const ItemWithPose = withPose(Item);
 
-  componentDidMount() {
-    this.setState({ isVisible: true });
-  }
-
-  render() {
-    return (
-      <Pose
-        ref={this.props.hostRef}
-        className={classNames(style.gridItem, style.youtube)}
-        pose={this.state.isVisible ? "visible" : "hidden"}
-      >
-        {chooseComponent(this.props)}
-      </Pose>
-    );
-  }
-}
-
-export default Item;
-
-/*
-const PosedComponent = posed(Item)({
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
-  initialPose: "hidden"
-});
-
-export default PosedComponent;
-//export default Item;
-*/
+export default withPose(Item);
--- client/src/components/withPose.js
+++ client/src/components/withPose.js
@@ -1,5 +1,31 @@
-import pose from "react-pose"
+import React from "react";
+import posed from "react-pose";
 
-const withPose = (component) => {
-  
-}
+const Pose = posed.div({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+  initialPose: "hidden"
+});
+
+const withPose = WrappedComponent => {
+  return class WithPose extends React.Component {
+    state = {
+      isVisible: false
+    };
+
+    componentDidMount() {
+      this.setState({ isVisible: true });
+    }
+
+    render() {
+      const { hostRef, ...ownProps } = this.props;
+      return (
+        <Pose ref={hostRef} pose={this.state.isVisible ? "visible" : "hidden"}>
+          <WrappedComponent {...ownProps} />
+        </Pose>
+      );
+    }
+  };
+};
+
+export default withPose;
