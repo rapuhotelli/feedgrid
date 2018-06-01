cd3ce83ffbdab63d10fc6d97ee8bfc577fde3ba5
rapuhotelli <jukka@lahetkangas.net>
2018-06-01 08:16:10 +0300

    Schloss

--- client/src/components/elements/Image.js
+++ client/src/components/elements/Image.js
@@ -1,46 +1,75 @@
 // @flow
 
 import React from "react";
-import { connect } from "react-redux";
-import { loadState } from "../../ducks/elements";
+// import { connect } from "react-redux";
+// import { loadState } from "../../ducks/elements";
+import styles from "./styles.pcss";
 
+/*
 type DispatchProps = {
   loadState: string => void
 };
+*/
 type OwnProps = {
   alt: ?string,
   data: Object
 };
-type Props = DispatchProps & OwnProps;
+type State = {
+  loading: boolean
+};
+type Props = OwnProps;
 
-class Image extends React.Component<Props> {
-  constructor(props) {
+class Image extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
-    this.props.loadState("PENDING");
+    // this.props.loadState("PENDING");
     console.log(this.props.data);
-    älä tee näin vain lataa sisäisesti
+    this.state = {
+      loading: true
+    };
   }
 
   loadSuccess() {
-    this.props.loadState("FULFILLED");
+    // this.props.loadState("FULFILLED");
+    this.setState({
+      loading: false
+    });
   }
 
   loadFailed() {
-    this.props.loadState("REJECTED");
+    // this.props.loadState("REJECTED");
+    this.setState({
+      loading: false
+    });
   }
 
   render() {
     return (
-      <img
-        alt={this.props.alt || ""}
-        src={this.props.data.url}
-        onLoad={this.loadSuccess.bind(this)}
-        onError={this.loadFailed.bind(this)}
-      />
+      <div
+        style={{
+          height: this.props.data.height,
+          width: this.props.data.width
+        }}
+        className={styles.imageContainer}
+      >
+        <img
+          alt={this.props.alt || ""}
+          src={this.props.data.url}
+          onLoad={this.loadSuccess.bind(this)}
+          onError={this.loadFailed.bind(this)}
+        />
+      </div>
     );
   }
 }
 
+export default Image;
+/*
+
+ */
+
+/*
 export default connect(null, {
   loadState
 })(Image);
+*/
